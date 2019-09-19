ed5fcbdd10a989ea62d9cdc5f6371166a69af9cb
devanshsa5a <mail2devansh@gmail.com>
2019-09-19 23:27:52 +0530

    bdcoe bot completed

--- PROJECT/bdcoe bot/model espeak/main.py
+++ PROJECT/bdcoe bot/model espeak/main.py
@@ -8,6 +8,9 @@ stemmer = LancasterStemmer()
 import random
 import json
 import pickle
+from nltk.stem import WordNetLemmatizer 
+  
+lemmatizer = WordNetLemmatizer()
 
 
 from gtts import gTTS 
@@ -26,48 +29,48 @@ except:
       labels = []
       docs_x = []
       docs_y = []
-      
+
       for intent in data["intents"]:
               for pattern in intent["patterns"]:
                   wrds = nltk.word_tokenize(pattern)
                   words.extend(wrds)
                   docs_x.append(wrds)
                   docs_y.append(intent["tag"])
-      
+
               if intent["tag"] not in labels:
                   labels.append(intent["tag"])
-      
-      words = [stemmer.stem(w.lower()) for w in words if w != "?"]
+
+      words = [lemmatizer.lemmatize(w.lower()) for w in words if w != "?"]
       words = sorted(list(set(words)))
-      
+
       labels = sorted(labels)
-      
+
       training = []
       output = []
-      
+
       out_empty = [0 for _ in range(len(labels))] 
-      
+
       for x, doc in enumerate(docs_x):
               bag = []
-      
-              wrds = [stemmer.stem(w.lower()) for w in doc if w not in "?"]
-      
+
+              wrds = [lemmatizer.lemmatize(w.lower()) for w in doc if w not in "?"]
+
               for w in words:
                   if w in wrds:
                       bag.append(1)
                   else:
                       bag.append(0)
-      
+
               output_row = out_empty[:]
               output_row[labels.index(docs_y[x])] = 1
-      
+
               training.append(bag)
               output.append(output_row)
-      
-      
+
+
       training = numpy.array(training)
       output = numpy.array(output)
-      
+
       with open("data.pickle", "wb") as f:
             pickle.dump((words, labels, training, output), f)
 
@@ -84,14 +87,14 @@ model = tflearn.DNN(net)
 try:
     model.load("model.tflearn")
 except:
-    model.fit(training, output, n_epoch=4000, batch_size=8, show_metric=True)
+    model.fit(training, output, n_epoch=10000, batch_size=8, show_metric=True)
     model.save("model.tflearn")
 
 def binary_of_data(s, words):
     bag = [0 for _ in range(len(words))]
 
     s_words = nltk.word_tokenize(s)
-    s_words = [stemmer.stem(word.lower()) for word in s_words]
+    s_words = [lemmatizer.lemmatize(word.lower()) for word in s_words]
 
     for se in s_words:
         for i, w in enumerate(words):
@@ -104,9 +107,16 @@ def binary_of_data(s, words):
 def chatbot():
     print("Start talking with the bdcoe bot (type quit to stop)!")
     while True:
+        print(" ")
         inp = input("bdcoe bot: ")
         if inp.lower() == "quit":
-            break
+          print(" ")
+          print("bye bye ")
+          print("__"*40)
+          print(" ")
+          final="espeak 'bye bye '"
+          os.system(final) 
+          break
 
         results = model.predict([binary_of_data(inp, words)])
         results_index = numpy.argmax(results)
@@ -117,8 +127,12 @@ def chatbot():
                 responses = tg['responses']
 
         text=random.choice(responses)
+        print(" ")
         print(text)
+        print("__"*40)
+        print(" ")
         final="espeak '{}'"
+
         os.system(final.format(text)) 
                
 
